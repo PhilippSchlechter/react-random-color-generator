3b1889d9d40243ff86eb75e5fe7321ea28d7650d
PhilippSchlechter <110399840+PhilippSchlechter@users.noreply.github.com>
2022-09-12 19:09:35 +0200

    Add second version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
+// on change?
 function App() {
+  const [color, setColor] = useState(randomColor());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <br />
+      <br />
+      <br />
+      <h1> React Random Color Generator</h1>
+      <br />
+      <br />
+      <br />
+      <div
+        style={{
+          width: 400,
+          height: 400,
+          margin: '0 auto',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 35,
+          border: '3px solid',
+          borderRadius: 300,
+          backgroundColor: color,
+        }}
+      >
+        Generated Color:
+        <br />
+        <br />
+        {color}
+        <br />
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <button
+        style={{
+          width: 80,
+          height: 80,
+          margin: '0 auto',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          border: '2px solid',
+          fontsize: 50,
+          borderRadius: 50,
+          backgroundColor: 'white',
+        }}
+        onClick={() => {
+          const color1 = randomColor();
+          setColor(color1);
+        }}
+      >
+        Generate
+      </button>
     </div>
   );
 }
